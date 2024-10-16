6a4e6882d8c2bd874222c3cd8211985f199b9357
harleennanwani <2023.harleen.nanwani@ves.ac.in>
2024-10-16 21:56:16 +0530

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+# hello_world.py
+
+# Print a greeting message
+print("Hello, World!")
+
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Print a personalized greeting
+print(f"Nice to meet you, {name}!")
+
+# Perform a simple calculation
+number = int(input("Enter a number to double it: "))
+doubled = number * 2
+print(f"The double of {number} is {doubled}.")
